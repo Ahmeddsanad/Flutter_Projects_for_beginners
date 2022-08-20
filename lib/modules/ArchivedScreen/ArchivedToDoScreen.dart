7a11ca67a0edb38e73446273626a083a6f4179cc
Ahmeddsanad <100171203+Ahmeddsanad@users.noreply.github.com>
2022-08-20 15:55:00 +0200

    beginning with flutter

--- lib/modules/ArchivedScreen/ArchivedToDoScreen.dart
+++ lib/modules/ArchivedScreen/ArchivedToDoScreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import '../../shared/component/components.dart';
 import '../../shared/cubit/cubit.dart';
 import '../../shared/cubit/states.dart';
