837d408f1fe0f0499192e54bccfb0d6b80197243
Ahmeddsanad <100171203+Ahmeddsanad@users.noreply.github.com>
2022-08-22 20:43:13 +0200

    beginning with flutter

--- lib/shared/component/components.dart
+++ lib/shared/component/components.dart
@@ -3,39 +3,33 @@ import 'package:flutter/material.dart';
 import 'package:project1/shared/cubit/cubit.dart';
 
 // ignore: non_constant_identifier_names
-Widget DefaultButton({
-  double width = double.infinity,
-  Color background = Colors.blue,
-  bool isUpperCase = true ,
-  double radius = 3.0 ,
-  required Function? function,
-  required String text
-}) => Container(
-  height: 50.0,
-  width: width,
-  child: MaterialButton(
-    onPressed: (){
-      function!();
-    },
-    child: Text(
-        isUpperCase ? text.toUpperCase() : text,
-        style:const TextStyle(
-            fontSize: 15.0,
-            color: Colors.white
+Widget DefaultButton(
+        {double width = double.infinity,
+        Color background = Colors.blue,
+        bool isUpperCase = true,
+        double radius = 3.0,
+        required Function? function,
+        required String text}) =>
+    Container(
+      height: 50.0,
+      width: width,
+      child: MaterialButton(
+        onPressed: () {
+          function!();
+        },
+        child: Text(
+          isUpperCase ? text.toUpperCase() : text,
+          style: const TextStyle(fontSize: 15.0, color: Colors.white),
         ),
-    ),
-  ),
-  decoration: BoxDecoration(
-    borderRadius: BorderRadius.circular(radius
-    ),
-    color: background
-  ),
-);
+      ),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(radius), color: background),
+    );
 
 Widget DefaultFormField({
   required TextEditingController Controller,
   required TextInputType type,
-  required Function validated ,
+  required Function validated,
   required String label,
   required IconData prefix,
   IconData? Suffix,
@@ -43,225 +37,145 @@ Widget DefaultFormField({
   Function? onSubmit,
   Function? onChange,
   Function? onTap,
-  bool isPassword = false ,
-}) => TextFormField(
-  controller: Controller,
-  keyboardType: type,
-  onFieldSubmitted: onSubmit != null ? onSubmit() : null,
-  onChanged: onChange != null ? onChange() : null,
-  onTap: onTap != null ? () => onTap() : null,
-  validator: (vale){
-    return validated(vale);
-  },
-  obscureText: isPassword,
-  decoration: InputDecoration(
-    // hintText: 'Email Address'
-    labelText: label,
-    prefixIcon: Icon(
-      prefix
-    ),
-    suffixIcon: Suffix != null ? IconButton(
-      icon: Icon(
-        Suffix,
-      ),
-      onPressed:(){
-        suffixPressed!();
+  bool isPassword = false,
+}) =>
+    TextFormField(
+      controller: Controller,
+      keyboardType: type,
+      onFieldSubmitted: onSubmit != null ? onSubmit() : null,
+      onChanged: onChange != null ? onChange() : null,
+      onTap: onTap != null ? () => onTap() : null,
+      validator: (vale) {
+        return validated(vale);
       },
-    )
-      : null ,
-    border: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(20.0),
-    ),
-  ),
-);
+      obscureText: isPassword,
+      decoration: InputDecoration(
+        // hintText: 'Email Address'
+        labelText: label,
+        prefixIcon: Icon(prefix),
+        suffixIcon: Suffix != null
+            ? IconButton(
+                icon: Icon(
+                  Suffix,
+                ),
+                onPressed: () {
+                  suffixPressed!();
+                },
+              )
+            : null,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+      ),
+    );
 
 //onTap != null ? () => onTap() : null,
-Widget BuildTaskItem(Map model,context) => Dismissible(
-  key: Key(model['id'].toString()),
-  child: Padding(
-  
-    padding: const EdgeInsets.all(20.0),
-  
-    child: Row(
-  
-      children: [
-  
-        CircleAvatar(
-  
-          radius: 40.0,
-  
-          child: Text(
-  
-            //${model['time']}
-  
-            '${model['time']}',
-  
-            style: TextStyle(
-  
-                fontWeight: FontWeight.bold,
-  
-                fontSize: 15.0,
-  
-                color: Colors.white
-  
+Widget BuildTaskItem(Map model, context) => Dismissible(
+      key: Key(model['id'].toString()),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          children: [
+            CircleAvatar(
+              radius: 40.0,
+              child: Text(
+                //${model['time']}
+
+                '${model['time']}',
+
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15.0,
+                    color: Colors.white),
+              ),
+              backgroundColor: Colors.black,
             ),
-  
-          ),
-  
-          backgroundColor: Colors.black,
-  
-        ),
-  
-        SizedBox(
-  
-          width: 10.0,
-  
-        ),
-  
-        Expanded(
-  
-          child: Column(
-  
-            crossAxisAlignment: CrossAxisAlignment.start,
-  
-            mainAxisSize: MainAxisSize.min,
-  
-            children: [
-  
-              Text(
-  
-                '${model['title']}',
-  
-                style: TextStyle(
-  
-                    fontSize: 18.0,
-  
-                    fontWeight: FontWeight.bold
-  
-                ),
-  
+            SizedBox(
+              width: 10.0,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    '${model['title']}',
+                    style:
+                        TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    '${model['date']}',
+                    style: TextStyle(color: Colors.grey, fontSize: 14.0),
+                  ),
+                ],
               ),
-  
-              Text(
-  
-                '${model['date']}',
-  
-                style: TextStyle(
-  
-                  color: Colors.grey,
-  
-                  fontSize: 14.0
-  
-                ),
-  
-              ),
-  
-            ],
-  
-          ),
-  
+            ),
+            SizedBox(
+              width: 10.0,
+            ),
+            IconButton(
+                onPressed: () {
+                  AppCubit.get(context).updateData(
+                    status: 'done',
+                    id: model['id'],
+                  );
+                },
+                icon: Icon(
+                  Icons.check_box,
+                  color: Colors.green,
+                )),
+            IconButton(
+                onPressed: () {
+                  AppCubit.get(context).updateData(
+                    status: 'archived',
+                    id: model['id'],
+                  );
+                },
+                icon: Icon(
+                  Icons.archive,
+                  color: Colors.black45,
+                )),
+          ],
         ),
-  
-        SizedBox(
-  
-          width: 10.0,
-  
-        ),
-  
-        IconButton(
-  
-            onPressed: ()
-  
-            {
-  
-            AppCubit.get(context).updateData(
-  
-                status: 'done',
-  
-                id: model['id'],
-  
-            );
-  
-            },
-  
-            icon: Icon(
-  
-                Icons.check_box,
-  
-              color: Colors.green,
-  
-            )
-  
-        ),
-  
-        IconButton(
-  
-            onPressed: ()
-  
-            {
-  
-              AppCubit.get(context).updateData(
-  
-                status: 'archived',
-  
-                id: model['id'],
-  
-              );
-  
-            },
-  
-            icon: Icon(
-  
-                Icons.archive,
-  
-              color: Colors.black45,
-  
-            )
-  
-        ),
-  
-      ],
-  
-    ),
-  
-  ),
-  onDismissed: (direction)
-  {
-     AppCubit.get(context).DeleteData(id: model['id'],);
-  },
-);
+      ),
+      onDismissed: (direction) {
+        AppCubit.get(context).DeleteData(
+          id: model['id'],
+        );
+      },
+    );
 
 Widget TasksBuilder({
   required List<Map> tasks,
-}) => ConditionalBuilder(
-  condition: tasks.isNotEmpty,
-  builder: (context) => ListView.separated(
-    itemBuilder: (context, index) => BuildTaskItem(tasks[index],context),
-    separatorBuilder: (context ,index) => Container(
-      width: double.infinity,
-      height: 1.0,
-      color: Colors.grey[300],
-    ),
-    itemCount: tasks.length,
-  ),
-  fallback: (context) => Center(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          Icons.menu,
-          size: 100.0,
-          color: Colors.grey,
+}) =>
+    ConditionalBuilder(
+      condition: tasks.isNotEmpty,
+      builder: (context) => ListView.separated(
+        itemBuilder: (context, index) => BuildTaskItem(tasks[index], context),
+        separatorBuilder: (context, index) => Container(
+          width: double.infinity,
+          height: 1.0,
+          color: Colors.grey[300],
         ),
-        Text(
-          'No Tasks Yet',
-          style: TextStyle(
-              fontSize: 16.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.black
-          ),
-        )
-      ],
-    ),
-  ),
-);
+        itemCount: tasks.length,
+      ),
+      fallback: (context) => Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.menu,
+              size: 100.0,
+              color: Colors.grey,
+            ),
+            Text(
+              'No Tasks Yet',
+              style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            )
+          ],
+        ),
+      ),
+    );
